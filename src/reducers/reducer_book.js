f6abf8b6b576912264f70e2eb74b0ffe48e4d1d4
ptavarez16 <ptavarez16@yahoo.com>
2018-08-27 21:42:50 -0400

    Add books reducer

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    {title: "Harry Potter and the Chamber of Secrets"},
+    {title: "Ender's Game"},
+    {title: "Eloquent JavaScript"},
+    {title: "Eloquent Ruby"}
+  ]
+}

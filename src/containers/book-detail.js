7628e847c72bd4a0fa884803fd0cef4d94ff9b64
ptavarez16 <ptavarez16@yahoo.com>
2018-08-28 22:04:32 -0400

    Remove comments

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
-    // Add a if state that checks if book is null. If so, return default text
     if(!this.props.book) {
       return (
         <div>Select A Book To Get Started!</div>
@@ -14,10 +13,6 @@ class BookDetail extends Component {
         <h4>Book Details:</h4>
         <div>Title: {this.props.book.title}</div>
         <div>Pages: {this.props.book.pages}</div>
-        {/*
-          Add a h3 with 'Details for:' as the text, followed by a div that
-          renders the book title, followed by another div that renders the book pages
-          */}
       </div>
     );
   }
